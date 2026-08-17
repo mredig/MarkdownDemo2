f7a8a5a2bf12566a1f4879bcbb19228347eb7035
Michael Redig <mredig@gmail.com>
2026-08-17 00:25:39 -0500

    refactor: live search only

--- Sources/libmarkdowndemo/Pages/PageTemplate.swift
+++ Sources/libmarkdowndemo/Pages/PageTemplate.swift
@@ -31,61 +31,7 @@ struct PageTemplate<Content: HTMLContentProvider, FooterContent: HTMLNode>: HTML
 
 			Link(href: URL(string: "/css/modern.css"), rel: .Link.stylesheet)
 
-			let script = """
-				var lastRequest = 0;
-				var lastAttempt = 0;
-
-				function showResult(str, updateAttempt) {
-					if (updateAttempt === undefined) updateAttempt = 1;
-					var date = new Date();
-					var currentTime = date.getTime();
-					var timeElapsed = currentTime - lastRequest;
-
-					var minimumPassingTime = 300;
-
-					if (str.length == 0) {
-						document.getElementById("markdownRepoLiveSearch").innerHTML = "";
-						document.getElementById("markdownRepoLiveSearch").style.border = "0px";
-						return;
-					}
-					if (timeElapsed < minimumPassingTime) {
-						if (updateAttempt == 1) {
-							lastAttempt = currentTime;
-						}
-						var thisAttempt = lastAttempt;
-						var timeout = minimumPassingTime + 5;
-						setTimeout(delayedSubmission, timeout, thisAttempt, str);
-						return;
-					}
-					lastAttempt = currentTime;
-					lastRequest = currentTime;
-					//    alert(lastRequest);
-
-					if (window.XMLHttpRequest) {
-						// code for IE7+, Firefox, Chrome, Opera, Safari
-						xmlhttp = new XMLHttpRequest();
-					} else {
-						// code for IE6, IE5
-						xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-					}
-					xmlhttp.onreadystatechange = function() {
-						if (this.readyState == 4 && this.status == 200) {
-							document.getElementById("markdownRepoLiveSearch").innerHTML = this.responseText;
-							document.getElementById("markdownRepoLiveSearch").style.border = "1px solid #A5ACB2";
-						}
-					}
-					xmlhttp.open("GET", "livesearch/?search=" + str, true);
-					xmlhttp.send();
-				}
-
-				function delayedSubmission(timeSubmitted, submission) {
-					if (timeSubmitted == lastAttempt) {
-						showResult(submission, false);
-					}
-				}
-				"""
-
-			Script(script)
+			Script(src: "/js/livesearch.js")
 		}
 	}
 
@@ -113,11 +59,6 @@ struct PageTemplate<Content: HTMLContentProvider, FooterContent: HTMLNode>: HTML
 		get throws {
 			try Body {
 				Form {
-					Button { "Go" }
-						.withType(.submit)
-						.addClass("btn")
-						.addClass("btn-default")
-
 					Div {
 						Input(inputType: .search, name: "search", id: "search")
 							.addClass("form-control")
@@ -132,9 +73,6 @@ struct PageTemplate<Content: HTMLContentProvider, FooterContent: HTMLNode>: HTML
 					Div().setID("markdownRepoLiveSearch")
 				}
 				.setClasses(["form-inline", "headerSearch"])
-				.withAction("/search")
-				.withMethod(.get)
-
 
 				let breadcrumbs = breadcrumbParagraph
 				breadcrumbs
---
+++ site_assets/public/js/livesearch.js
@@ -0,0 +1,51 @@
+var lastRequest = 0;
+var lastAttempt = 0;
+
+function showResult(str, updateAttempt) {
+	if (updateAttempt === undefined) updateAttempt = 1;
+	var date = new Date();
+	var currentTime = date.getTime();
+	var timeElapsed = currentTime - lastRequest;
+
+	var minimumPassingTime = 300;
+
+	if (str.length == 0) {
+		document.getElementById("markdownRepoLiveSearch").innerHTML = "";
+		document.getElementById("markdownRepoLiveSearch").style.border = "0px";
+		return;
+	}
+	if (timeElapsed < minimumPassingTime) {
+		if (updateAttempt == 1) {
+			lastAttempt = currentTime;
+		}
+		var thisAttempt = lastAttempt;
+		var timeout = minimumPassingTime + 5;
+		setTimeout(delayedSubmission, timeout, thisAttempt, str);
+		return;
+	}
+	lastAttempt = currentTime;
+	lastRequest = currentTime;
+	//    alert(lastRequest);
+
+	if (window.XMLHttpRequest) {
+		// code for IE7+, Firefox, Chrome, Opera, Safari
+		xmlhttp = new XMLHttpRequest();
+	} else {
+		// code for IE6, IE5
+		xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+	}
+	xmlhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			document.getElementById("markdownRepoLiveSearch").innerHTML = this.responseText;
+			document.getElementById("markdownRepoLiveSearch").style.border = "1px solid #A5ACB2";
+		}
+	}
+	xmlhttp.open("GET", "livesearch/?search=" + str, true);
+	xmlhttp.send();
+}
+
+function delayedSubmission(timeSubmitted, submission) {
+	if (timeSubmitted == lastAttempt) {
+		showResult(submission, false);
+	}
+}
